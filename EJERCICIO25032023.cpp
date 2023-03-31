98eaa9bb4212dd36e4988a8ccc4d49d652695e98
Jyrn-007 <uomashu14@gmail.com>
2023-03-31 00:57:07 -0600

    arreglo de la variables char

--- EJERCICIO25032023.cpp
+++ EJERCICIO25032023.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #define _CRT_SECURE_NO_WARNINGS
 #define gopri(a,b,c) gotoxy(a,b); printf(c);
-#define ASCENDENTE 12
+#define ASCENDENTE 1
 #define DESCENDENTE 0
 #include <stdlib.h> 
 #include <stdio.h> 
@@ -20,17 +20,18 @@ void gotoxy(int x, int y)
 }
 struct nodo {
 	int Dpi;
- string nombre;
- string sexo;
- string dirección;
- string Tipo_alergia;
- string fechanacimiento;
- string estadocivil;
+	char a[100];
+  char *nombre=a;;
+ char *sexo=a;
+ char *dirección=a;
+ char *Tipo_alergia=a;
+ char *fechanacimiento=a;
+  char *estadocivil=a;
  int ttelefonofijo;
  int ttelefonomovil;
- string correoelectronico;
- string brigrada;
- string fechainiciobrigada;
+ char *correoelectronico = a;
+ char *brigrada = a;
+ char *fechainiciobrigada = a;
  nodo* sgt;
 
 
@@ -40,17 +41,20 @@ void agrega(nodo** cab, nodo** no , nodo** sex,  nodo** dir, nodo** tipal, nodo*
 
 int main()
 {
-	nodo* c = NULL, * d = NULL, * e = NULL, * f = NULL, * g= NULL, * h = NULL, * i = NULL, * j = NULL, * k = NULL, * l = NULL, * m = NULL, * n = NULL, * o = NULL;  //puntero de cabecera, y puntero de fin de lista
+	nodo* c = NULL, * d = NULL, * e = NULL, * g = NULL, * h = NULL, * i = NULL, * j = NULL, * k = NULL, * l = NULL, * m = NULL, * n = NULL, * o = NULL, *f = NULL;  //puntero de cabecera, y puntero de fin de lista
 	int opcion;
 	opcion = 1; do {
 		system("cls");
-		gopri(10, 10, " 1. Ingresa un dato numerico entero");
+		gopri(10, 10, " 1. Registrarse en la base de datos");
+		gopri(10, 12, " 2. Control de las inscripciónes a los diferentes comandos y brigadas de la fuerza naval");
 		gopri(10, 14, " (0) para finalizar");
 		gopri(10, 15, "Ingresa la opcion");
 		cin >> opcion;
 		switch (opcion) {
 		case 0: exit(0); break;
-		case 1: agrega(&c, &d, &e, &f, &g, &h, &i, &j, &k, &l, &m, &n, &o); break;
+		case 1: agrega(&c, &d, &e, &g, &h, &i, &j, &k, &l, &m, &n, &o, &f); break;
+	
+
 		}
 	} while (opcion != 0);
 	system("PAUSE");
@@ -58,21 +62,23 @@ int main()
 }
 
 void agrega(nodo** cab, nodo** no, nodo** sex, nodo** dir, nodo** tipal, nodo** fena, nodo** esciv, nodo** telf, nodo** telmov, nodo** cor, nodo** brig, nodo** fechinbri, nodo** fin) {
-	
-	int Dp; cout << "ingrese su numero de DPI" << endl;  cin >> Dp;
-	string nom; cout << "ingrese su nombre completo" << endl;  cin >> nom;
-	string sexoo; cout << "ingrese su sexo" << endl;  cin >> sexoo;
-	string domicilio; cout << "ingrese su nombre completo" << endl; cin >> domicilio;
-	string tipoalergia; cout << "ingrese su nombre completo" << endl; cin >> tipoalergia;
-	string fechnacimineto; cout << "ingrese su nombre completo" << endl; cin >> fechnacimineto;
-	string eestadocivil; cout << "ingrese su nombre completo" << endl; cin >> eestadocivil;
-	int telefonofijo; cout << "ingrese su nombre completo" << endl; cin >> telefonofijo;
-	int telefonomovil; cout << "ingrese su nombre completo" << endl; cin >> telefonomovil;
-	string correo; cout << "ingrese su nombre completo" << endl; cin >> correo;
-	string brigadaper; cout << "ingrese su nombre completo" << endl; cin >> brigadaper;
-	string fechainbrigada; cout << "ingrese su nombre completo" << endl; cin >> fechainbrigada;
 
-	if ( ((*cab), (* no), (* sex), (* dir), (* tipal), ( * fena), (* esciv), (* telf), (* telmov),  (* cor), (* brig), (* fechinbri)) == NULL) {
+	char a[100];
+	int Dp; cout << "ingrese su numero de DPI" << endl;  cin >> Dp;
+	char *nom=a; cout << "ingrese su nombre completo" << endl;  cin >> nom;
+     char *sexoo=a; cout << "ingrese su sexo" << endl; cin >> sexoo;
+
+	char *domicilio=a; cout << "4.DirecciOn de domicilio" << endl; cin >> domicilio;
+	char* tipoalergia = a; cout << "Tipo de alergias" << endl; cin >> tipoalergia;
+	 char* fechnacimineto = a; cout << "Fecha de nacimiento" << endl; cin >> fechnacimineto;
+	 char* eestadocivil = a; cout << "Estado civil" << endl; cin >> eestadocivil;
+	int telefonofijo; cout << "Teléfono fijo," << endl; cin >> telefonofijo;
+	int telefonomovil; cout << "Teléfono móvil" << endl; cin >> telefonomovil;
+	char* correo = a; cout << "Correo electrónico," << endl; cin >> correo;
+	char* brigadaper = a; cout << "Brigada a la que pertenece," << endl; cin >> brigadaper;
+	char* fechainbrigada = a; cout << "Fecha de inicio en la brigada." << endl; cin >> fechainbrigada;
+
+
 		*cab = new nodo;
 		*no = new nodo;
 		*sex = new nodo;
@@ -87,6 +93,8 @@ void agrega(nodo** cab, nodo** no, nodo** sex, nodo** dir, nodo** tipal, nodo** 
 		*fechinbri= new nodo;
 		*fin = new nodo;
 		(*cab)->Dpi = Dp;
+		(*cab)->sgt =NULL;
+
 		(*no)->nombre = nom;
 		(*sex)->sexo = sexoo;
 		(*dir)->dirección = domicilio;
@@ -100,13 +108,8 @@ void agrega(nodo** cab, nodo** no, nodo** sex, nodo** dir, nodo** tipal, nodo** 
 		(*fechinbri)->fechainiciobrigada = fechainbrigada;
 		(*fechinbri)->sgt = NULL;
 		(*fin) = (*fechinbri);
-	}
+	
 
-	else {
-		(*fin)->sgt = new nodo;
-		(*fin) = (*fin)->sgt;
-		(*fin)->sgt = NULL;
-	}
-
+	
 }
 
